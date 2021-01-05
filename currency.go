00f65cd9b507d5f6dcf77d789c5823c7cfad7541
andrew <0x9171@gmail.com>
2021-01-05 23:26:06 +0300

    upd

--- currency.go
+++ currency.go
@@ -14,3 +14,23 @@ type Currency struct {
 
 	Users []string
 }
+
+func ByAlpha3(v string) *Currency {
+	for _, c := range List {
+		if c.Alpha3 == v {
+			return &c
+		}
+	}
+
+	return nil
+}
+
+func ByNumeric(v string) *Currency {
+	for _, c := range List {
+		if c.Numeric == v {
+			return &c
+		}
+	}
+
+	return nil
+}
